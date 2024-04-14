d6ea668bccb456a907af693278571c032a646902
Sonu Kumar Yadav <yadavsonukumar036@gmail.com>
2024-04-14 20:38:09 +0530

    footer

--- Frontend/quadbtech/src/App.jsx
+++ Frontend/quadbtech/src/App.jsx
@@ -8,7 +8,20 @@ function App() {
     <div className="min-h-screen bg-gray-800">
       <Header />
       <Stats />
+      <hr className="border-gray-600 my-4" />
       <Data />
+      <hr className="border-gray-600 my-4" />
+      <footer className="flex flex-col items-center justify-center text-center text-gray-500 text-sm mt-4 p-5">
+        <hr className="w-full mb-4 border-gray-600" />
+        <div className="w-full flex justify-between ">
+          <p className="mb-2 font-bold text-white">Copyright © 2019 HodlInfo.com</p>
+          <a href="/support" className="underline  no-underline text-white font-bold">Support</a>
+        </div>
+        <hr className="w-full mb-4 border-gray-600" />
+        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mt-4">
+          Add HodlInfo to Home Screen
+        </button>
+      </footer>
     </div>
   );
 }
